31a34f240880ebc9e5073f665e18741a8f42b249
Christian Grabowski <christiang11754@gmail.com>
2016-12-28 00:21:30 -0500

    refactor go funcs for jail to handle the memory allocation

--- jail.go
+++ jail.go
@@ -2,27 +2,39 @@
 
 package jail
 
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <sys/param.h>
+// #include <sys/jail.h>
+// #include <sys/types.h>
+// #include <netinet/in.h>
+// #include <arpa/inet.h>
+// #define IN_C false
 // #include "jailwrapper.h"
 import "C"
-
-import (
-	"runtime/debug"
-	"unsafe"
-)
+import "fmt"
 
 type Jail struct {
-	Wrapper unsafe.Pointer
+	Wrapper C.JailWrapper
 }
 
 func New(cmd string) *Jail {
-	var jail *C.JailWrapper
-	jail = C.new_jail_wrapper(C.CString(cmd))
-	debug.PrintStack()
-	return &Jail{
-		Wrapper: unsafe.Pointer(jail),
-	}
+	cJail := (*C.struct_jail)(C.calloc(6, C.sizeof_jail))
+	C.set_path(cJail, C.CString("/tmp/"))
+	C.set_hostname(cJail, C.CString("test_jail"))
+	C.set_jailname(cJail, C.CString("test_jail"))
+	inet := (*C.struct_in_addr)(C.calloc(1, C.sizeof_struct_in_addr))
+	C.inet_aton(C.CString("0.0.0.0"), inet)
+	C.set_ip4(cJail, inet)
+	jid := C.jail_init(cJail)
+	fmt.Println(jid)
+	pid := C.jail_exec(C.CString(cmd), jid)
+	wrapper := (*C.struct_JailWrapper)(C.calloc(3, C.sizeof_JailWrapper))
+	C.set_wrapper_pid(wrapper, pid)
+	uid := (C.uid_t)(C.getuid())
+	C.set_wrapper_uid(wrapper, uid)
+	return &Jail{}
 }
 
 func (j *Jail) Destroy() {
-	C.destroy(j.Wrapper)
 }
